352fc8a79c509c994b44018e47019e4af3ff1f5a
kornel <novakoka66@gmail.com>
2025-03-22 19:51:02 +0100

    renomme un fichier

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import CarteProfil from "../components/CarteProfil"
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <carteProfil
+          nom="Novajkay"
+          age="48"
+          description="Dévellopeur en pleine progession sur React et Next.js"
+        />
+      </div>
+    </>
+  );
+}
